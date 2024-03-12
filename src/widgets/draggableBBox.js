549e7b2056bd13e746e48149e099f0d64b554e75
Shruti Varade <shrutivarade27@gmail.com>
2024-03-12 12:58:13 -0400

    Segment Fixed with rectangle offset

--- src/widgets/draggableBBox.js
+++ src/widgets/draggableBBox.js
@@ -28,26 +28,38 @@ export class DraggableBBox extends Widget {
   }
 
   mouseDown(e) {
+
+    var rect = e.currentTarget.getBoundingClientRect()
+
+    this.isDrawing = true;
+
+    this.x1 = e.clientX * window.devicePixelRatio - rect.left * window.devicePixelRatio;
+    this.y1 = e.clientY * window.devicePixelRatio - rect.top * window.devicePixelRatio;
+
     this.startX = e.offsetX;
     this.startY = e.offsetY;
-    this.isDrawing = true;
-    this.x1 = e.clientX  * window.devicePixelRatio ;
-    this.y1 = e.clientY  * window.devicePixelRatio ;
-    console.log("Mouse down at: ", this.x1, this.y1);
+
+    console.log("Mouse down at:", this.x1, this.y1);
   }
 
   mouseMove(e) {
     if (!this.isDrawing) return;
     this.ctx.clearRect(0, 0, this.overlayCanvas.width, this.overlayCanvas.height); // Clear canvas
     let width = e.offsetX - this.startX;
-    let height = e.offsetY  - this.startY;
+    let height = e.offsetY - this.startY;
+    console.log("Mouse Move at:", this.x1, this.y1);
     this.ctx.strokeRect(this.startX , this.startY, width, height);
+
   }
 
   mouseUp(e) {
     this.isDrawing = false;
-    this.x2 = e.clientX  * window.devicePixelRatio ;
-    this.y2 = e.clientY  * window.devicePixelRatio ;
+
+    var rect = e.currentTarget.getBoundingClientRect()
+
+    this.x2 = e.clientX * window.devicePixelRatio - rect.left * window.devicePixelRatio;
+    this.y2 = e.clientY * window.devicePixelRatio - rect.top * window.devicePixelRatio;
+
     console.log("Mouse up at: ", this.x2, this.y2);
     this.storeCoordinates();
   }
@@ -55,9 +67,14 @@ export class DraggableBBox extends Widget {
   mouseOut() {
     if (this.isDrawing) {
       // Store coordinates also when the mouse accidentally leaves the canvas
-      this.x2 = e.clientX  * window.devicePixelRatio ;
-      this.y2 = e.clientY  * window.devicePixelRatio ;
+
+      var rect = e.currentTarget.getBoundingClientRect()
+
+      this.x2 = e.clientX * window.devicePixelRatio - rect.left * window.devicePixelRatio;
+      this.y2 = e.clientY * window.devicePixelRatio - rect.top * window.devicePixelRatio;
+      
       console.log("Mouse out at: ", this.x2, this.y2);
+
       this.storeCoordinates();
     }
     this.isDrawing = false;
@@ -66,7 +83,6 @@ export class DraggableBBox extends Widget {
   storeCoordinates() {
     let topleft = { x: this.x1, y: this.y1 };
     let bottomright = { x: this.x2, y: this.y2 };
-    // console.log("Top Left:", topleft, "Bottom Right:", bottomright);
     this.callback(topleft, bottomright); // Invoke the callback with the coordinates
   }
 }
